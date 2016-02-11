dcaab6da4850e2e74a70b5dd84c4d1e9b647b967
Laurent Maillet <laurent.maillet@zenika.com>
2016-02-12 00:46:31 +0100

    update server start

--- index.js
+++ index.js
@@ -70,7 +70,9 @@ app.use(function (err, req, res) {
   });
 });
 
+app.set('port', process.env.PORT || 3000);
 
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
 
-
-module.exports = app;
