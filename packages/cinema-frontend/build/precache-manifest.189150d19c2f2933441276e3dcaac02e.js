7ea0f98af3a3fcaaaae2540df639e221e5dd8866
Laurent Maillet <nebounet@gmail.com>
2018-10-28 21:46:18 +0800

    feat: update build

---
+++ packages/cinema-frontend/build/precache-manifest.189150d19c2f2933441276e3dcaac02e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81c3564b9cfa1ea64d3a",
+    "url": "/static/js/main.81c3564b.chunk.js"
+  },
+  {
+    "revision": "69f7f5c665e45143fe91",
+    "url": "/static/js/1.69f7f5c6.chunk.js"
+  },
+  {
+    "revision": "81c3564b9cfa1ea64d3a",
+    "url": "/static/css/main.11fe88ca.chunk.css"
+  },
+  {
+    "revision": "c1c895edc2a58b5e30d3f92f18a0e813",
+    "url": "/index.html"
+  }
+];
