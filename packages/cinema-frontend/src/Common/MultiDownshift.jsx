e8aceed0320554063db5ccad93e148371729d1fd
Laurent Maillet <nebounet@gmail.com>
2018-10-29 21:55:28 +0800

    fix: downshift initialization + modal css

--- packages/cinema-frontend/src/Common/MultiDownshift.jsx
+++ packages/cinema-frontend/src/Common/MultiDownshift.jsx
@@ -3,6 +3,15 @@ import Downshift from "downshift";
 
 export class MultiDownshift extends React.Component {
   state = { selectedItems: this.props.selectedItems || [] };
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.selectedItems !== this.props.selectedItems &&
+      this.props.selectedItems !== this.state.selectedItems
+    ) {
+      this.setState({ selectedItems: this.props.selectedItems || [] });
+    }
+  }
   stateReducer = (state, changes) => {
     switch (changes.type) {
       case Downshift.stateChangeTypes.keyDownEnter:
