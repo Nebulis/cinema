d86d0180fac47e9718f464814a0b2ac85884976e
Laurent Maillet <nebounet@gmail.com>
2018-11-30 20:06:15 +0800

    feat: build

---
+++ packages/cinema-frontend/build/precache-manifest.c618ff4e940b3e0243f7be285c8718c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "69c496d04cbd61c932a2",
+    "url": "/static/js/main.69c496d0.chunk.js"
+  },
+  {
+    "revision": "bbc258cd610d3497ceda",
+    "url": "/static/js/2.bbc258cd.chunk.js"
+  },
+  {
+    "revision": "69c496d04cbd61c932a2",
+    "url": "/static/css/main.7c64cdb7.chunk.css"
+  },
+  {
+    "revision": "73c6afa4487d6d544afb17cfa92222f4",
+    "url": "/index.html"
+  }
+];
--- packages/cinema-frontend/build/service-worker.js
+++ packages/cinema-frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.139d070821077e883f86f400318c0633.js"
+  "/precache-manifest.c618ff4e940b3e0243f7be285c8718c5.js"
 );
 
 workbox.clientsClaim();
