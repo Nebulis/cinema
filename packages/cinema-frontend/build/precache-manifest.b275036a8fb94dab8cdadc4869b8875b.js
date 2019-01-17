a6203b330e128fefc46c8f7caa0cfe388221559a
Nebulis <nebounet@gmail.com>
2019-01-18 00:04:48 +0800

    feat: hooksie

---
+++ packages/cinema-frontend/build/precache-manifest.b275036a8fb94dab8cdadc4869b8875b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "eed30389c1e808ab125d",
+    "url": "/static/js/main.eed30389.chunk.js"
+  },
+  {
+    "revision": "4f0e52f47047fb445b0e",
+    "url": "/static/js/2.4f0e52f4.chunk.js"
+  },
+  {
+    "revision": "eed30389c1e808ab125d",
+    "url": "/static/css/main.0009f957.chunk.css"
+  },
+  {
+    "revision": "f84ea83248cdbc765143ed5707705f15",
+    "url": "/index.html"
+  }
+];
--- packages/cinema-frontend/build/service-worker.js
+++ packages/cinema-frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a8b04dbccf1fdcc30963f0d14470d64.js"
+  "/precache-manifest.b275036a8fb94dab8cdadc4869b8875b.js"
 );
 
 workbox.clientsClaim();
