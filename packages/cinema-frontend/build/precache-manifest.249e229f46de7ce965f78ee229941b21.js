0e6aa46a3c0bdaccd12ee9e0ddb7d62d36caa975
Nebulis <nebounet@gmail.com>
2019-06-11 16:28:54 +0800

    feat: use short resume from allocine

---
+++ packages/cinema-frontend/build/precache-manifest.249e229f46de7ce965f78ee229941b21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "5de0f12795d70a2bc223",
+    "url": "/static/js/main.5de0f127.chunk.js"
+  },
+  {
+    "revision": "4f0e52f47047fb445b0e",
+    "url": "/static/js/2.4f0e52f4.chunk.js"
+  },
+  {
+    "revision": "5de0f12795d70a2bc223",
+    "url": "/static/css/main.ca271a67.chunk.css"
+  },
+  {
+    "revision": "df5840442557c38534b51965491318be",
+    "url": "/index.html"
+  }
+];
