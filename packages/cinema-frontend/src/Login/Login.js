d3827bb8f7d304931c49395847cce91cc29639c4
Nebulis <nebounet@gmail.com>
2020-07-17 23:24:37 +0800

    fix: wtf

--- packages/cinema-frontend/src/Login/Login.js
+++ packages/cinema-frontend/src/Login/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 import { LOGIN_FAILED, withUser } from "./UserContext";
 import { login } from "../firebase/configuration";
